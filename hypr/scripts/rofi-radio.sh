a41f87be3fa89aabb7761ad8668b96a8c4188daf
lassejep <lasse.jepsen@hotmail.com>
2024-04-05 00:44:08 +0200

    added DR p1 to radio

---
+++ hypr/scripts/rofi-radio.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+ARGS="--no-video"
+notification(){
+	notify-send "Playing now: " "$@"
+}
+
+menu(){
+	printf "1. Lofi Girl <lofi>\n"
+    printf "2. Theprimeagen <stream>\n"
+    printf "3. DR P1 <radio>\n"
+}
+
+main() {
+    choice=$(menu | rofi -dmenu | cut -d. -f1)
+
+    case $choice in
+        1)
+            notification "Lofi Girl🎶";
+            URL="https://youtu.be/jfKfPfyJRdk"
+            ADDITIONAL_ARGS="--volume=60"
+            break
+            ;;
+        2)
+            notification "Theprimeagen Stream";
+            URL="https://www.twitch.tv/theprimeagen"
+            ADDITIONAL_ARGS=""
+            break
+            ;;
+        3)
+            notification "DR P1";
+            URL="https://live-icy.gslb01.dr.dk/A/A03H.mp3"
+            additional_args=""
+            break
+            ;;
+
+        esac
+        mpv $ARGS --title="radio-mpv" $URL $ADDITIONAL_ARGS
+    }
+
+pkill -f radio-mpv || main
+notify-send "Radio stopped"

9f6177ce59c89324b85cbd4aed8662a55c3416ef
lassejep <lasse.jepsen@hotmail.com>
2024-05-29 12:33:30 +0200

    changed a tmux template

--- tmux/templates/ljk.bash
+++ tmux/templates/ljk.bash
@@ -3,17 +3,16 @@ session=ljk
 tmux rename-window -t ${session}:1 "client"
 tmux split-window -v -t ${session}:client.1 -l 8
 tmux split-window -h -t ${session}:client.1
-tmux send-keys -t ${session}:client.1 'cd ~/ws/ljk && nvim .' C-m
-tmux send-keys -t ${session}:client.2 'cd ~/ws/ljk && nvim .' C-m
-tmux send-keys -t ${session}:client.3 'cd ~/ws/ljk && clear' C-m
+tmux send-keys -t ${session}:client.1 'cd ~/ws/ljk && nvim ljkey.py' C-m
+tmux send-keys -t ${session}:client.2 'cd ~/ws/ljk && clear' C-m
 tmux select-pane -t ${session}:client.1
 
 tmux new-window -t ${session}:2 -n "server"
 tmux split-window -v -t ${session}:server.1 -l 8
 tmux split-window -h -t ${session}:server.1
-tmux send-keys -t ${session}:server.1 'cd ~/ws/ljk_server && nvim .' C-m
-tmux send-keys -t ${session}:server.2 'cd ~/ws/ljk_server && nvim .' C-m
-tmux send-keys -t ${session}:server.3 'cd ~/ws/ljk_server && clear' C-m
+tmux send-keys -t ${session}:server.1 'cd ~/ws/ljk && nvim server.py' C-m
+tmux send-keys -t ${session}:server.2 'cd ~/ws/ljk && clear' C-m
+tmux send-keys -t ${session}:server.3 'cd ~/ws/ljk && clear' C-m
 tmux select-pane -t ${session}:server.1
 
 tmux new-window -t ${session}:3 -n "misc"

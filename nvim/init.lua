e35f9d0bca6e73d3b2566d9a976e34fd85727d8e
lassejep <lasse.jepsen@hotmail.com>
2024-06-15 08:16:10 +0200

    added keybind to markdown preview nvim plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -583,8 +583,21 @@ require("lazy").setup({
 		build = "cd app && yarn install",
 		init = function()
 			vim.g.mkdp_filetypes = { "markdown" }
+			vim.cmd([[
+			function OpenMarkdownPreview (url)
+				execute "silent ! firefox --new-window " . a:url
+			endfunction
+			]])
+			vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
 		end,
 		ft = { "markdown" },
+		keys = {
+			{
+				"<leader>mp",
+				vim.cmd.MarkdownPreview,
+				desc = "Markdown [P]review",
+			},
+		},
 	},
 }, {
 	ui = {
